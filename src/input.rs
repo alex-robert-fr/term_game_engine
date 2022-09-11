ec35562a9aa493b9ded00c735ffd06aada495586
Alex <contact.alexrobert04@gmail.com>
2022-09-12 01:11:55 +0200

    FEAT: Input Keyboard

---
+++ src/input.rs
@@ -0,0 +1,42 @@
+use std::time::Duration;
+
+use crossterm::event::{
+    poll, read, Event, KeyCode, KeyEvent, KeyEventKind, KeyEventState, KeyModifiers,
+};
+
+pub struct Keyboard {}
+
+impl Keyboard {
+    pub fn get_last_key_press(&self) -> Option<Key> {
+        match poll(Duration::from_millis(0)) {
+            Ok(_) => match read().unwrap() {
+                Event::Key(event) => Some(self.translate_key(event)),
+                _ => None,
+            },
+            Err(_) => None,
+        }
+    }
+
+    fn translate_key(&self, key: KeyEvent) -> Key {
+        match key {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::NONE,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE,
+            }
+            | KeyEvent {
+                code: KeyCode::Char('Q'),
+                modifiers: KeyModifiers::SHIFT,
+                kind: KeyEventKind::Press,
+                state: KeyEventState::NONE,
+            } => Key::Q,
+            _ => Key::Unknown,
+        }
+    }
+}
+
+pub enum Key {
+    Q,
+    Unknown,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use input::Keyboard;
+
+pub mod input;
+
 pub struct Engine {
     pub fps: u8,
     pub game: Game,
@@ -9,12 +14,14 @@ impl Engine {
         Engine { fps, game: Game {  }, state: State::new() }
     }
 
-    pub fn run<G>(&mut self, mut game: G) where G: FnMut(&mut Game) {
+    pub fn run<G>(&mut self, mut game: G) where G: FnMut(&mut Game, &mut State) {
+        enable_raw_mode().unwrap();
         while !self.state.exit {
             println!("Run Engine");
-            game(&mut self.game);
+            game(&mut self.game, &mut self.state);
             println!("After Run Engine");
         }
+        disable_raw_mode().unwrap();
     }
 }
 
@@ -23,11 +30,12 @@ pub struct Game {
 }
 
 pub struct State {
-    exit: bool
+    pub keyboard: Keyboard,
+    pub exit: bool
 }
 
 impl State {
     pub fn new() -> Self {
-        State { exit: false }
+        State { exit: false, keyboard: Keyboard {  } }
     }
 }
