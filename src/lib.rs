5c77e418d92ba4598d47f8d0f3c7122a160e417b
Alex <contact.alexrobert04@gmail.com>
2022-09-12 00:23:23 +0200

    FEAT: Add run function in Engine

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 pub struct Engine {
-    pub fps: u8
+    pub fps: u8,
+    pub game: Game
 }
 
 impl Engine {
     pub fn new(fps: u8) -> Self {
-        Engine { fps }
+        Engine { fps, game: Game {  } }
     }
+
+    pub fn run<G>(&mut self, mut game: G) where G: FnMut(&mut Game) {
+        println!("Run Engine");
+        game(&mut self.game);
+        println!("After Run Engine");
+    }
+}
+
+pub struct Game {
+    
 }
