63c7c4fcdf2cac12c37bd01e3838e3ba69d1a729
Alex <contact.alexrobert04@gmail.com>
2022-09-12 00:29:01 +0200

    FEAT: Add State in Engine

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,33 @@
 pub struct Engine {
     pub fps: u8,
-    pub game: Game
+    pub game: Game,
+    pub state: State
 }
 
 impl Engine {
     pub fn new(fps: u8) -> Self {
-        Engine { fps, game: Game {  } }
+        Engine { fps, game: Game {  }, state: State::new() }
     }
 
     pub fn run<G>(&mut self, mut game: G) where G: FnMut(&mut Game) {
-        println!("Run Engine");
-        game(&mut self.game);
-        println!("After Run Engine");
+        while !self.state.exit {
+            println!("Run Engine");
+            game(&mut self.game);
+            println!("After Run Engine");
+        }
     }
 }
 
 pub struct Game {
     
+}
+
+pub struct State {
+    exit: bool
+}
+
+impl State {
+    pub fn new() -> Self {
+        State { exit: false }
+    }
 }
